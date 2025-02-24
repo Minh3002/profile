32af8f58ab3b18696e8d7987f9ea8d501fbc4158
Minh3002 <truonggiaminh123@gmail.com>
2025-02-24 21:16:12 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
-const path = require('path')
- 
-module.exports = {
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Xuất thành web tĩnh để GitHub Pages có thể chạy
+  basePath: "/my-profile", // Đặt theo tên repo GitHub của bạn (thay bằng tên repo của bạn)
+  assetPrefix: "/my-profile/", // Giúp Next.js load asset đúng trên GitHub Pages
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      'media.dev.to',
+      'avatars.githubusercontent.com',
+      'github-readme-stats.vercel.app',
+      'github-readme-streak-stats.herokuapp.com',
+      'stardev.io',
+      'github-profile-summary-cards.vercel.app',
+      'github-profile-trophy.vercel.app',
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+  },
+};
+
+module.exports = nextConfig;
